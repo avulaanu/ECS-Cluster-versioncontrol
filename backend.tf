c1b88dad9b22f8dcbde372a33744cdfa60bd185b
avulaanu <108398096+avulaanu@users.noreply.github.com>
2022-10-11 16:26:56 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@ terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
     organization = "anasuya"
+    token = "GAxjKLA4zo9RWQ.atlasv1.D5pWT9yGuTrodlMc5aiXKOHzoTNXId9IkBu5NujwqhWJlr1JZ2uZyXARjmvjCoVqJcQ"
     workspaces {
       name = "Anasuya"
     }
